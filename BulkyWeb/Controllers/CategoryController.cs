5010aebb8de738b28f3ea12e8d2b6ab57732fbfe
madoomino <sasa6521d@gmail.com>
2024-04-14 00:08:44 -0400

    adding category & view categories & validation

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController(ApplicationDbContext db) : Controller
+    {
+        private readonly ApplicationDbContext _db = db;
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = [.. _db.Categories];
+            return View(objCategoryList);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Name and Display Order cannot be the same");
+            }
+            {
+                ModelState.AddModelError("Name", "Name and Display Order cannot be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+        }
+    }
+}
