f45e00204c6ed6a6af7af9e5784de2c0eab95f63
madoomino <sasa6521d@gmail.com>
2024-04-14 04:38:14 -0400

    adding all crud operations & notification toaster

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -26,17 +26,55 @@ namespace BulkyWeb.Controllers
             {
                 ModelState.AddModelError("Name", "Name and Display Order cannot be the same");
             }
-            {
-                ModelState.AddModelError("Name", "Name and Display Order cannot be the same");
-            }
+
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["created"] = "Category Saved Successfully";
                 return RedirectToAction("Index");
 
             }
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["edited"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(Category obj)
+        {
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["deleted"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
         }
     }
 }
